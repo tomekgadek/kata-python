2653b5a2287cdfd187ad0d628c52d243d31c1fb5
tomekgadek <gadektomek@gmail.com>
2025-04-12 21:50:43 +0200

    kata-poly wstepna implementacja klasy PolyError i Poly

---
+++ math/poly.py
@@ -0,0 +1,17 @@
+"""
+    poly.py: implementacja wielomianiu.
+"""
+
+
+class PolyError(Exception):
+    """
+        Wyj�tek dla klasy FracPoly.
+    """
+    def __init__(self, message):
+        self.message = message
+
+class Poly:
+    pass
+
+if __name__ == "__main__":
+    pass
