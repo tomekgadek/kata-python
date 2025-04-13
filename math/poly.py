fe3630055de6d50ff0d8c6f66e990fd99eb90c69
tomekgadek <gadektomek@gmail.com>
2025-04-13 14:44:49 +0200

    kata-poly implementacja __init__() i __str__()

--- math/poly.py
+++ math/poly.py
@@ -2,6 +2,7 @@
     poly.py: implementacja wielomianiu.
 """
 
+from frac import *
 
 class PolyError(Exception):
     """
@@ -11,7 +12,31 @@ class PolyError(Exception):
         self.message = message
 
 class Poly:
-    pass
+    
+    def __init__(self, x=0, y=1, n=0):
+        """
+            Tworzy wielomian c*x^n, gdzie c = x/y (wg Sedgewicka).
+            x: licznik współczynnika
+            y: mianownik współczynnika
+            n: stopień wielomianu
+        """
+        if y == 0:
+            raise PolyError("Dzielenie przez 0!")
+        self.size = n + 1
+        self.p = [Frac() for _ in range(self.size)]
+        self.p[-1] = Frac(x, y)
 
+    def __str__(self):
+        """
+            Zwraca stringa reprezentującego wielomian.
+        """
+        result = ", ".join([str(data) for data in self.p])
+        return f"[{result}]"
+    
 if __name__ == "__main__":
-    pass
+    
+    fp1 = Poly(1, 2, 3)
+    fp2 = Poly(1, 4, 2)
+
+    assert f"{fp1}" == "[0, 0, 0, 1/2]"
+    assert f"{fp2}" == "[0, 0, 1/4]"
