c132ef9e6871735a3a7ae554cb81a3dbd8f3c75e
tomekgadek <gadektomek@gmail.com>
2025-04-14 22:38:21 +0200

    kata-poly dodawanie wielomianow

--- math/poly.py
+++ math/poly.py
@@ -12,7 +12,7 @@ class PolyError(Exception):
         self.message = message
 
 class Poly:
-    
+
     def __init__(self, x=0, y=1, n=0):
         """
             Tworzy wielomian c*x^n, gdzie c = x/y (wg Sedgewicka).
@@ -32,11 +32,36 @@ class Poly:
         """
         result = ", ".join([str(data) for data in self.p])
         return f"[{result}]"
+    def __add__(self, other):
+        """
+            Dodaje wielomian do innego wielomianu.
+        """
+        if isinstance(other, (int, float)):
+            other = Poly(other)
+        
+        max_size = max(self.size, other.size)
+        result = Poly(0, 1, max_size - 1)
+        
+        for i in range(self.size):
+            result.p[i] += self.p[i]
+        
+        for i in range(other.size):
+            result.p[i] += other.p[i]
+        
+        return result
+    __radd__ = __add__
     
 if __name__ == "__main__":
     
     fp1 = Poly(1, 2, 3)
     fp2 = Poly(1, 4, 2)
 
+    # str
     assert f"{fp1}" == "[0, 0, 0, 1/2]"
     assert f"{fp2}" == "[0, 0, 1/4]"
+
+    # add
+    assert f"{fp1 + fp2}" == "[0, 0, 1/4, 1/2]"
+    assert f"{fp1 + 1}" == "[1, 0, 0, 1/2]"
+    assert f"{2 + fp1}" == "[2, 0, 0, 1/2]"
+    assert f"{1 + fp1 + 2 + fp2}" == "[3, 0, 1/4, 1/2]"
