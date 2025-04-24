50a7ae1ca998d85739a2f25448f61ecef633ff0a
tomekgadek <gadektomek@gmail.com>
2025-04-24 21:25:01 +0200

    kata-tkinker 2 etap refaktoryzacji

--- tkinker/game.py
+++ tkinker/game.py
@@ -2,9 +2,6 @@
     game.py: implementacja prostej gry typu 'paper, rock, scissors' w Tkinterze
 """
 
-# TODO: implementacja wymaga jeszcze refaktoryzacji.
-# TODO: komunikaty są niejasne
-
 import tkinter as tk
 import random
 
@@ -15,17 +12,21 @@ class GameApp(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.title("Paper, Rock, Scissors")
-        self.geometry("300x150")
+        self.title("Paper | Rock | Scissors")
+        self.geometry("500x250")
 
-        self.available_choices = ["paper", "rock", "scissors"]
+        self.available_choices = {
+            "paper": "📃",
+            "rock": "🪨",
+            "scissors": "✂️"
+        }
 
-        self.label = Label(self, text="Let's play paper, rock, scissors!", font=40)
+        self.label = Label(self, text="Zagrajmy w Papier, Kamień, Nożyce!", font=40)
         self.label.pack(pady=10)
 
-        Button(self, text="📃 Paper", command=lambda: self.play_cmd("paper")).pack(pady=5)
-        Button(self, text="🤘 Rock", command=lambda: self.play_cmd("rock")).pack(pady=5)
-        Button(self, text="✂️ Scissors", command=lambda: self.play_cmd("scissors")).pack(pady=5)
+        Button(self, text="📃 Papier", command=lambda: self.play_cmd("paper")).pack(pady=5)
+        Button(self, text="🪨 Kamień", command=lambda: self.play_cmd("rock")).pack(pady=5)
+        Button(self, text="✂️ Nożyce", command=lambda: self.play_cmd("scissors")).pack(pady=5)
 
     def play(self, player, cpu):
         win_with = {"paper": "rock", "rock": "scissors", "scissors": "paper"}
@@ -37,18 +38,30 @@ class GameApp(tk.Tk):
             return False
 
     def choice(self):
-        return random.choice(self.available_choices)
+        return random.choice(list(self.available_choices.keys()))
 
     def play_cmd(self, player):
         cpu = self.choice()
         is_user_winner = self.play(player, cpu)
 
+        player_choice = self.available_choices[player]
+        cpu_choice = self.available_choices[cpu]
+
         if is_user_winner is None:
-            self.label.config(text="Tie! Try again!", foreground="blue")
+            self.label.config(
+                text=f"Remis! GRACZ({player_choice}) vs CPU({cpu_choice}). Spróbuj ponownie!",
+                foreground="blue"
+            )
         elif is_user_winner:
-            self.label.config(text="You win... Let's play again", foreground="green")
+            self.label.config(
+                text=f"Wygrałeś! GRACZ({player_choice}) vs CPU({cpu_choice}). Zagrajmy jeszcze raz!",
+                foreground="green"
+            )
         else:
-            self.label.config(text="I win, I win!", foreground="red")
+            self.label.config(
+                text=f"Przegrałeś! GRACZ({player_choice}) vs CPU({cpu_choice}). Spróbuj ponownie!",
+                foreground="red"
+            )
 
 
 if __name__ == "__main__":
